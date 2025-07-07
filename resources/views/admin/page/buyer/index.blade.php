ce1b4cdda765857268951f4e08c1cbf63a24858f
dwiprakoso <dwiprakoso015@gmail.com>
2025-07-07 11:25:43 +0700

    fix : rev 2

--- resources/views/admin/page/buyer/index.blade.php
+++ resources/views/admin/page/buyer/index.blade.php
@@ -132,7 +132,52 @@
                                 </span>
                             </div>
                             <div class="d-flex align-items-center py-3">
-                                {{ $buyers->links() }}
+                                <!--begin::Pages-->
+                                <ul class="pagination">
+                                    {{-- Previous Page Link --}}
+                                    @if ($buyers->onFirstPage())
+                                        <li class="page-item previous disabled">
+                                            <a href="#" class="page-link">
+                                                <i class="previous"></i>
+                                            </a>
+                                        </li>
+                                    @else
+                                        <li class="page-item previous">
+                                            <a href="{{ $buyers->previousPageUrl() }}" class="page-link">
+                                                <i class="previous"></i>
+                                            </a>
+                                        </li>
+                                    @endif
+
+                                    {{-- Pagination Elements --}}
+                                    @foreach ($buyers->getUrlRange(1, $buyers->lastPage()) as $page => $url)
+                                        @if ($page == $buyers->currentPage())
+                                            <li class="page-item active">
+                                                <a href="#" class="page-link">{{ $page }}</a>
+                                            </li>
+                                        @else
+                                            <li class="page-item">
+                                                <a href="{{ $url }}" class="page-link">{{ $page }}</a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+
+                                    {{-- Next Page Link --}}
+                                    @if ($buyers->hasMorePages())
+                                        <li class="page-item next">
+                                            <a href="{{ $buyers->nextPageUrl() }}" class="page-link">
+                                                <i class="next"></i>
+                                            </a>
+                                        </li>
+                                    @else
+                                        <li class="page-item next disabled">
+                                            <a href="#" class="page-link">
+                                                <i class="next"></i>
+                                            </a>
+                                        </li>
+                                    @endif
+                                </ul>
+                                <!--end::Pages-->
                             </div>
                         </div>
                     @endif
