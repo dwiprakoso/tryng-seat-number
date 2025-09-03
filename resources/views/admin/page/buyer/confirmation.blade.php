558c187a0216bfa66941fdb6f057e0bee5329a80
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-03 22:16:05 +0700

    fix : price on payment confirmation

--- resources/views/admin/page/buyer/confirmation.blade.php
+++ resources/views/admin/page/buyer/confirmation.blade.php
@@ -174,10 +174,10 @@
                                 <table class="table table-row-bordered table-row-gray-100 align-middle gs-0 gy-3">
                                     <tbody>
                                         <tr>
-                                            <td class="fw-semibold text-muted">Harga Tiket ({{ $buyer->quantity }} x
-                                                {{ number_format($buyer->ticket_price, 0, ',', '.') }} )</td>
+                                            <td class="fw-semibold text-muted">Harga Tiket ( {{ $buyer->quantity }} x
+                                                {{ number_format($buyer->ticket->price, 0, ',', '.') }} )</td>
                                             <td class="text-end fw-bold">Rp
-                                                {{ number_format($buyer->ticket_price * $buyer->quantity, 0, ',', '.') }}
+                                                {{ number_format($buyer->ticket_price, 0, ',', '.') }}
                                             </td>
                                         </tr>
                                         {{-- <tr>
